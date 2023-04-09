b6cb74294a149ac1d2e6fc2f4881f0a6acecb424
Ivan <us.ivan05@gmail.com>
2023-04-09 21:31:39 +0800

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace MatrixEffect
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            matrix.Start();
+            for (int i = 0; i<20; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixEffect
+{
+    //Matrix color: 15 - white, 10 light green, 2 - green
+    internal class Matrix
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
