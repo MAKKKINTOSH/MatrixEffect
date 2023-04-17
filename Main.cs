f7b30a198492b0c7ebae156138568357bb959912
Ivan <us.ivan05@gmail.com>
2023-04-17 17:11:28 +0800

    some main.cs changes

--- Main.cs
+++ Main.cs
@@ -6,7 +6,10 @@ namespace MatrixEffect
     {
         private static AutoResetEvent wait = new AutoResetEvent(true);
         public static void Main(string[] args)
-        {
+        {   
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            Console.Clear();
             Matrix matrix = new Matrix();
             matrix.Start();
         }
